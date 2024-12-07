8713b0dd06b1caf671dc542c496e580fad5ba5c5
unknown <ilarrumbide10@gmail.com>
2024-12-07 14:09:12 -0300

    cambios en app.py en el endpoint predictions

--- tp_amq2_17co2024/models/model_loader.py
+++ tp_amq2_17co2024/models/model_loader.py
@@ -18,7 +18,7 @@ class ModelLoader():
             self.version_model_ml = int(model_data_mlflow.version)
         except:
             # If there is no registry in MLflow, open the default model
-            file_ml = open('/app/files/best_xgboost_model.pkl', 'rb')
+            file_ml = open('../models/best_xgboost_model.pkl', 'rb')
             self.model_ml = pickle.load(file_ml)
             file_ml.close()
             self.version_model_ml = 0
